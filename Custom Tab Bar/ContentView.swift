2274d70c9816fc7b7d900fc28cdd917fcc77ff7c
LuckDuracell <lukedru@yahoo.com>
2021-07-16 20:46:48 -0700

    Initial Commit

---
+++ Custom Tab Bar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Tab Bar
+//
+//  Created by Luke Drushell on 7/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom Tab Bar/Custom_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Tab_BarApp.swift
+//  Custom Tab Bar
+//
+//  Created by Luke Drushell on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
